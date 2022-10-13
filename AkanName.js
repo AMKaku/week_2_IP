c2891914ce588b4116b8523d558741de47e26ed3
Muli <arthurkaku@gmail.com>
2022-10-13 11:59:47 +0300

    ammendments on JS

--- AkanName.js
+++ AkanName.js
@@ -1,31 +1,65 @@
-const DAYS_OF_THE_WEEK = ["Sunday","Monday","Tuesday","wednesday","Thursday","Friday","Saturday"]
-const MALE_NAMES = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
-const FEMALE_NAMES = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+function akanNames() {
+    var daysOfTheWeek = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ]; // shows days of the week
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-function calculateNameAndDay(century,year,month,day,gender){
-    var IndexOfTheDay = Math.round((((century/4) -2*century-1) + ((5*year/4)) + ((26*(month+1)/10)) + day)) % 7;
-    var dayTheUserWasBorn = DAYS_OF_THE_WEEK[IndexOfTheDay];
-var name = null;
+    var dateInput = prompt("Enter date of birth: YYYY-MM-DD:"); // requests user to put date of birth in indicated format.
 
-if (gender === 'm'){
-    name = MALE_NAMES[IndexOfTheDay];
-} else if (gender === 'f'){
-    name = FEMALE_NAMES[IndexOfTheDay];
-} else{
-     alert("Invalid gender");
+    var birthDate = new Date(dateInput); // fetches date from the user input
+
+    var dateBorn = birthDate.getDay(); // displays date of birth as  a number
+    var birthMonth = birthDate.getMonth();
+    var birthYear = birthDate.getFullYear();
+    var femaleGender = femaleNames[dateBorn]; //converts number into name of day
+    var maleGender = maleNames[dateBorn]; // converts number into name of day born
+
+
+    /*if (genderInput === "Male") {
+      alert("Your Akan name is" + maleGender);
+    } else {
+      alert("Your Akan name is" + femaleGender);
+    }*/
+
+    /*if ( genderInput ==="MALE" || genderInput==="FEMALE" || genderInput==="M" || genderInput==="F") {
+        alert("continue with in put");
+    } 
+    else {
+        alert("invalid");
+    }*/
+
+    if (
+        dateBorn >= 0 &&
+        dateBorn <= 6 &&
+        birthMonth >= 0 &&
+        birthMonth <= 11 &&
+        birthYear >= 1000
+    ) {
+        var genderInput = prompt("Enter your gender: Male or Female").toUpperCase;// validates genderInput
+        if (
+            genderInput !== "MALE" ||
+            genderInput !== "FEMALE" ||
+            genderInput !== "M" ||
+            genderInput !== "F"
+        ) {
+            if (genderInput !== "M" || genderInput !== "F" || genderInput !== "MALE" || genderInput !== "FEMALE") {
+                alert("Your Akan name is " + maleGender);
+            } else {
+                alert("Your Akan name is " + femaleGender);
+            }
+        } else {
+            alert("invalid gender use M/MALE or F/FEMALE");
+        }
+    } else {
+        alert("wrong date put date in this format YYYY-MM-DD");
+    }
 }
 
-var list = [dayTheUserWasBorn,name];
-return list;
-}
-function querry(){
-    var userInputYear = parseInt(prompt("enter the year"));;
-    var userInputMonth = parseInt(prompt("enter the month"));;
-    var userInputDay = parseInt(prompt("enter the day"));;
-    var userInputGender = prompt("enter the gender");;
-    var userInputCentury = parseInt(userInputYear.toString().substring(0,2));
-    var output = calculateNameAndDay(userInputCentury,userInputYear,userInputMonth,userInputDay,userInputGender);
-    var outputElement = document.getElementById("output");
-    outputElement.innerHTML = "You were born on: " + output[0] + ". Your Akan name is:" + output[1];
-}
 
